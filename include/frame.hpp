46c1374f56e0991b02d47bffdb3b61d96294be29
Jacky Zhao <yzhao8056@gmail.com>
2026-06-07 15:35:27 -0400

    Feat: frames

--- include/frame.hpp
+++ include/frame.hpp
@@ -1,1 +1,20 @@
 // frame.hpp
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include "value.hpp"
+
+class Frame {
+public:
+    explicit Frame(std::shared_ptr<Frame> parent = nullptr);
+    void define(const std::string& name, Value value);
+    Value lookup(const std::string& name) const;
+    void mutate(const std::string& name, Value value);
+
+private:
+    std::shared_ptr<Frame> parent_;
+    std::unordered_map<std::string, Value> mapping_;
+};
--- src/frame.cpp
+++ src/frame.cpp
@@ -1,1 +1,43 @@
 // frame.cpp
+#include "frame.hpp"
+#include "errors.hpp"
+
+#include <utility>
+
+Frame::Frame(std::shared_ptr<Frame> parent)
+    : parent_(std::move(parent)) {}
+
+void Frame::define(const std::string& name, Value value) {
+    mapping_[name] = std::move(value);
+}
+
+Value Frame::lookup(const std::string& name) const {
+    const auto it = mapping_.find(name);
+
+    if (it != mapping_.end()) {
+        return it->second;
+    }
+
+    if (parent_ != nullptr) {
+        return parent_->lookup(name);
+    }
+
+    throw SchemeNameError("Unknown name: " + name);
+}
+
+// Finds and mutates the value if exists.
+void Frame::mutate(const std::string& name, Value value) {
+    const auto it = mapping_.find(name);
+
+    if (it != mapping_.end()) {
+        it->second = std::move(value);
+        return;
+    }
+
+    if (parent_ != nullptr) {
+        parent_->mutate(name, value);
+        return;
+    }
+
+    throw SchemeNameError("Unknown name: " + name);
+}
